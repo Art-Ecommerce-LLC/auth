175df6777c75126220b92b2cbaa22828d9e374e6
myersben9 <myersben9@outlook.com>
2024-09-17 11:01:15 -0700

    otp

---
+++ app/(auth)/otp/OTPSession.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { OTPForm } from "@/components/form/OTPForm"
+import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/hooks/use-toast"
+import { useRouter } from "next/navigation"
+
+export default function OTPPage({ user, session, sessionMFA }: { user: Record<string,any>, session: Record<string,any>, sessionMFA: boolean }) {
+  const router = useRouter();
+  console.log(sessionMFA, user, session);
+  if (sessionMFA) {
+    router.push('/dashboard');
+  }
+
+
+  async function resendEmail() {
+    const { toast } = useToast();
+    // Send email to user with backend POST request
+    const response = await fetch('/api/auth/resendOTP', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        })
+    const responseData = await response.json()
+    if (responseData.error) {
+        toast({
+            variant: "destructive",
+            description: "Something Went Wrong",
+        })
+    } else {
+        toast({
+            variant: "success",
+            description: "OTP has been resent",
+            })
+        }
+}
+
+  return (
+    <main className="flex flex-col min-h-screen w-full items-center justify-center bg-primary text-primary-foreground">
+        <div className="w-full max-w-96 min-w-80 p-2">
+            <OTPForm/>
+            <Button onClick={resendEmail} className="w-80 mt-1">Resend OTP</Button>
+        </div>        
+    </main>
+  );
+}
